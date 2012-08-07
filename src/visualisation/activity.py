ce5a07b2dee252232b9a57d4f33c0c23bee03bc8
Tom Close <tclose@oist.jp>
2012-08-07 13:03:40 +0900

    Started to debug why there are no spikes in the mossy fibers

--- src/visualisation/activity.py
+++ src/visualisation/activity.py
@@ -21,19 +21,18 @@ import NeuroTools.signals as sig #@UnresolvedImport
 PROJECT_PATH = os.path.normpath(os.path.join(os.path.realpath(__file__), '..', '..', '..'))
 ACTIVITY_PATH = os.path.join(PROJECT_PATH, 'activity')
 
-parser = argparse.ArgumentParser(description='A script to ')
-parser.add_argument('cell', help='The name of the cell to plot.')
-parser.add_argument('run', help='Name of the run to load the data from')
-parser.add_argument('variable', default='spikes', help='The name of variable to plot.')
+parser = argparse.ArgumentParser(description='A script to plot spike activity')
+parser.add_argument('filename', help='The file to plot the spikes from')
 parser.add_argument('--time_start', type=float, default=0.0, help='The start of the plot')
 parser.add_argument('--time_stop', type=float, default=100.0, help='The stop of the plot')
 args = parser.parse_args()
 
 length = args.time_stop - args.time_start
 
-if args.variable == 'spikes':
-    filename = os.path.join(ACTIVITY_PATH, args.run, 'fabios_network.out.' + args.cell + '.' + args.variable)
-    spikes_n_ids = numpy.loadtxt(filename)
+variable = args.filename.split('.')[-1]
+
+if variable == 'spikes':
+    spikes_n_ids = numpy.loadtxt(args.filename)
     spikes = spikes_n_ids[:, 0]
     ids = spikes_n_ids[:, 1]
     fig = plt.figure()
@@ -44,7 +43,6 @@ if args.variable == 'spikes':
     max_id = numpy.max(ids)
     ax.set_xlim(args.time_start - 0.05 * length, args.time_stop + 0.05 * length)
     ax.set_ylim(-2, max_id + 2)
-
     plt.show()
 else:
-    raise Exception ("Unrecognised variable '%s'" % args.variable)
+    raise Exception ("Unrecognised variable '%s'" % variable)
