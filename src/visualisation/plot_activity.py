e381c24fc001859c01f08349e9f2a4cf0fd5bbb9
Tom Close <tclose@oist.jp>
2012-08-22 12:33:08 +0900

    Added voltage trace plot (with minimum increment to reduce number of lines required) to plot_activity

--- src/visualisation/plot_activity.py
+++ src/visualisation/plot_activity.py
@@ -25,6 +25,7 @@ parser = argparse.ArgumentParser(description='A script to plot spike activity')
 parser.add_argument('filename', help='The file to plot the spikes from')
 parser.add_argument('--time_start', type=float, default=None, help='The start of the plot')
 parser.add_argument('--time_stop', type=float, default=None, help='The stop of the plot')
+parser.add_argument('--v_incr', type=float, default=0.1, help='The minimum increment required before the next step in the voltage trace is plotted')
 args = parser.parse_args()
 
 
@@ -59,6 +60,31 @@ if variable == 'spikes':
     ax.set_xlim(time_start - 0.05 * length, time_stop + 0.05 * length)
     ax.set_ylim(-2, max_id + 2)
     # Show the spikes
+    plt.show()
+elif variable == 'v':
+    v_trace = numpy.loadtxt(args.filename)
+    if not v_trace.shape[0]:
+        print "No trace was loaded from file"
+        sys.exit(0)
+    v = v_trace[:,0]
+    ids = numpy.unique(v_trace[:,1])
+    num_ids = len(ids)
+    num_times = v.shape[0] / num_ids
+    if not num_times:
+        print "No times loaded from voltage trace file"
+        sys.exit(0)
+    voltages = numpy.reshape(v, (num_times, num_ids))
+    times = numpy.arange(0, num_times)
+    for ID in ids:
+        indices_to_plot = [0]
+        prev_v = voltages[0, ID]
+        for i, v in enumerate(voltages[1:, ID]):
+            if abs(v - prev_v) > args.v_incr:
+                indices_to_plot.append(i)
+                prev_v = v
+        voltages_to_plot = voltages[indices_to_plot, ID]
+        times_to_plot = times[indices_to_plot]
+        plt.plot(times_to_plot, voltages_to_plot)
     plt.show()
 else:
     raise Exception ("Unrecognised variable '%s'" % variable)
