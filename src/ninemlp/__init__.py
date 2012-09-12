3c7a5b2ed0df551f61a53760d1b551b7f4d2c0a3
Tom Close <tclose@oist.jp>
2012-09-12 17:13:49 +0900

    removing build_mode from package in favour of locally passed variables

--- src/ninemlp/__init__.py
+++ src/ninemlp/__init__.py
@@ -20,6 +20,7 @@ import os
 __version__ = "0.0.1"
 
 BUILD_MODE_OPTIONS = ['lazy', 'force', 'compile_only']
+DEFAULT_BUILD_MODE = 'lazy'
 
 SRC_PATH_ENV_NAME = 'NINEMLP_SRC_PATH'
 BUILD_MODE_NAME = 'NINEMLP_BUILD_MODE'
@@ -30,23 +31,6 @@ if SRC_PATH_ENV_NAME in os.environ: # NINEMLP_SRC_PATH has been set as an enviro
 else: # Otherwise determine from path to this module
     SRC_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 
-def set_build_mode(build_mode):
-    """
-    Sets the build mode for the whole system (including pyNN components) 
-    """
-    if build_mode != 'lazy' and build_mode != 'compile_only' and build_mode != 'force':
-        raise Exception("Unrecognised build mode '%s' (valid options are 'lazy', 'compile_only' or 'force')" % build_mode)
-    _BUILD_MODE = build_mode
-    
-def get_build_mode():
-    return _BUILD_MODE
-
-if BUILD_MODE_NAME in os.environ:
-    _BUILD_MODE = set_build_mode(os.environ[BUILD_MODE_NAME])
-else:
-    _BUILD_MODE = 'lazy'
-    
-print _BUILD_MODE
 
 if MPI_NAME in os.environ:
     import mpi4py #@UnresolvedImport
