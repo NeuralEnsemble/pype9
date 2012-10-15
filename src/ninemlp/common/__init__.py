04c1dc04b02cff8e85fa3e4864328475e322ea2a
Tom Close <tclose@oist.jp>
2012-10-15 09:56:04 +0900

    refactored simulationDefaults to simulationParams

--- src/ninemlp/common/__init__.py
+++ src/ninemlp/common/__init__.py
@@ -123,14 +123,14 @@ class NetworkMLHandler(XMLHandler):
                 self.network.flags[attrs['id']] = True
             else:
                 self.network.flags[attrs['id']] = False
-        elif self._opening(tag_name, attrs, 'simulationDefaults', parents=['parameters']): pass
-        elif self._opening(tag_name, attrs, 'temperature', parents=['simulationDefaults']):
+        elif self._opening(tag_name, attrs, 'simulationParams', parents=['parameters']): pass
+        elif self._opening(tag_name, attrs, 'temperature', parents=['simulationParams']):
             self.network.sim_params['temperature'] = float(attrs['value'])
-        elif self._opening(tag_name, attrs, 'timeStep', parents=['simulationDefaults']):
+        elif self._opening(tag_name, attrs, 'timeStep', parents=['simulationParams']):
             self.network.sim_params['timestep'] = float(attrs['value'])
-        elif self._opening(tag_name, attrs, 'minDelay', parents=['simulationDefaults']):
+        elif self._opening(tag_name, attrs, 'minDelay', parents=['simulationParams']):
             self.network.sim_params['min_delay'] = float(attrs['value'])
-        elif self._opening(tag_name, attrs, 'maxDelay', parents=['simulationDefaults']):
+        elif self._opening(tag_name, attrs, 'maxDelay', parents=['simulationParams']):
             self.network.sim_params['max_delay'] = float(attrs['value'])
         elif self._opening(tag_name, attrs, 'population', parents=['network']):
             self.pop_id = attrs['id']
