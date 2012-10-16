ad4491cd30f4217bcb822b24fcd2ce90df830195
Tom Close <tclose@oist.jp>
2012-10-16 17:05:00 +0900

    Added module for compiling nest modules

---
+++ src/ninemlp/nest/build.py
@@ -0,0 +1,13 @@
+"""
+
+  This module contains functions for building and loading nest modules
+
+  @author Tom Close
+
+"""
+
+#######################################################################################
+#
+#    Copyright 2012 Okinawa Institute of Science and Technology (OIST), Okinawa, Japan
+#
+#######################################################################################
