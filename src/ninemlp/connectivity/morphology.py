02fbbcf0076e1ad3409f4769aab5581d4ea405b8
Tom Close <tclose@oist.jp>
2012-11-29 11:37:40 +0900

    Initialised Tree.Mask up until voxel centres

--- src/ninemlp/connectivity/morphology.py
+++ src/ninemlp/connectivity/morphology.py
@@ -14,46 +14,78 @@
 import numpy as np
 import collections
 import xml.sax
-from ninemlp import XMLHandler, SRC_PATH
-from copy import copy
+from ninemlp import XMLHandler
 
 class Tree(object):
 
     def __init__(self, root, point_count):
+        """
+        Initialised the Tree object
+        
+        @param root [NeurolucidaXMLHandler.Branch]: The root branch of a tree loaded from a Neurolucida XML tree description
+        @param point_count [int]: The number of tree points that were loaded from the XML description
+        """
         self.root = root
-        self.points = np.zeros((point_count, 4))
+        self.points = np.zeros((point_count, 3))
+        self.diams = np.zeros(point_count)
         self._flatten_points(self.root)
-        self.tiled_diams = np.tile(self.diams, (1, 3))
-        self.min_bounds = np.squeeze(np.min(self.points[:, 0:2] - self.tiled_diams))
-        self.max_bounds = np.squeeze(np.max(self.points[:, 0:2] + self.tiled_diams))
+        tiled_diams = np.tile(self.diams.reshape((-1,1)), (1,3))
+        self.min_bounds = np.squeeze(np.min(self.points - tiled_diams, axis=0))
+        self.max_bounds = np.squeeze(np.max(self.points + tiled_diams, axis=0))
         self.mask = None
         self.mask_vox = None
 
     def _flatten_points(self, branch, point_index=0):
         """
-        A recursive algorithm to flatten the loaded tree into a np array
+        A recursive algorithm to flatten the loaded tree into a numpy array of points used in the
+        tree constructor.
+        
+        @param branch[NeurolucidaXMLHandler.Branch]
         """
         for point in branch.points:
-            self.points[point_index, :] = point
+            self.points[point_index, :] = point[0:3]
+            self.diams[point_index] = point[3]
             point_index += 1
         for branch in branch.sub_branches:
             point_index = self._flatten_points(branch, point_index)
         return point_index
 
-    def create_mask(self, vox=(1, 1, 1)):
-        vox = np.array(vox)
-        offsets = np.array(np.floor(self.min_bounds / vox), dtype=np.int)
-        limits = np.array(np.ceil(self.max_bounds / vox), dtype=np.int)
-        dims = limits - offsets
-        mask = np.array(dims)
-        scaled_points = copy(self.points[:, 0:2])
-        diam_extents = copy(self.tiled_diams)
-        for dim_i in xrange(3):
-            scaled_points[:, dim_i] /= vox[dim_i]
-            diam_extents[:, dim_i] /= vox[dim_i]
-        for scaled_point, diam_extent in zip(scaled_points, diam_extents):
-            # from extent to extent paint the voxels
-            pass
+    class Mask(object):
+        """
+        A mask containing the probability of finding a synaptic/presynaptic location at voxels
+        (3D pixels) of arbitrary width that divide up the bounding box of a dendritic/axonal tree
+        """
+        
+        def __init__(self, tree, vox_size):
+            """
+            Initialises the mask from a given Neurolucida tree and voxel size
+            
+            @param tree [Tree]: A loaded Neurolucida tree
+            @param vox_size [float]: The requested voxel sizes with which to divide up the mask with
+            """
+            self.vox_size = np.asarray(vox_size)
+            # Save int versions of the offset and limits for convenient matching with other arrays
+            self.start_index = np.array(np.floor(tree.min_bounds / self.vox_size), dtype=np.int)
+            self.finish_index = np.array(np.ceil(tree.max_bounds / self.vox_size), dtype=np.int)
+            # Get the offset and limits of the mask, which are lower and upper corners of the voxel
+            # array that cover the bounds of the tree
+            self.offset = self.start_index * self.vox_size
+            self.limit = self.finish_index * self.vox_size
+            # Initialise the actual numpy array to hold the values
+            self.dim = self.finish_index - self.start_index
+            self._mask = np.array(self.dim)
+            # Create an "open" grid of the voxel centres for efficient (and convenient) 
+            # calculation of the distance from voxel centres to the tree points. (Note since it is 
+            # open it only takes up O(dim[0] + dim[1] + dim[2]) memory instead of 
+            # O(dim[0] * dim[1] * dim[2]), which it would take if it were dense.)
+            grid_start = self.offset + self.vox_size / 2.0
+            grid_finish = self.limit - self.vox_size / 2.0
+            self.X, self.Y, self.Z = np.ogrid[grid_start[0]:grid_finish[0]:(self.dim[0] * 1j),
+                                              grid_start[1]:grid_finish[1]:(self.dim[1] * 1j),
+                                              grid_start[2]:grid_finish[2]:(self.dim[2] * 1j)]
+            for point, diam in zip(tree.points, tree.diams):
+                # from extent to extent paint the voxels
+                pass
 
 class NeurolucidaXMLHandler(XMLHandler):
     """
@@ -93,7 +125,7 @@ class NeurolucidaXMLHandler(XMLHandler):
     def endElement(self, tag_name):
         if self._closing(tag_name, 'tree', required_attrs=[('type', 'Dendrite')]):
             assert(len(self.open_branches) == 1)
-            self.trees.append(Tree(self.open_branches.pop()), self.point_count)
+            self.trees.append(Tree(self.open_branches.pop(), self.point_count))
             self.tree_point_count = 0
         elif self._closing(tag_name, 'branch', parents=[('tree', 'branch')]):
             self.open_branches.pop()
@@ -107,8 +139,10 @@ def read_NeurolucidaXML(filename):
     parser.parse(filename)
     return handler.trees
 
+
 if __name__ == '__main__':
-    import os.path
-    trees = read_NeurolucidaXML(os.path.normpath(os.path.join(SRC_PATH, '..', 'morph', 'Purkinje',
-                                                  'xml', 'GFP_P60.1_slide7_2ndslice-HN-FINAL.xml')))
-    trees[0].create_mask()
+    from os.path import normpath, join
+    from ninemlp import SRC_PATH
+    trees = read_NeurolucidaXML(normpath(join(SRC_PATH, '..', 'morph', 'Purkinje', 'xml', 
+                                              'GFP_P60.1_slide7_2ndslice-HN-FINAL.xml')))
+    mask = Tree.Mask(trees[0], (10,10,10))
