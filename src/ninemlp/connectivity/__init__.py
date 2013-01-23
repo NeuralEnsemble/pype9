4fbc0b10908e3566e6d21754ca27278c09e28f31
Tom Close <tclose@oist.jp>
2013-01-23 13:35:46 +0900

    got correct rotation matrix for transformation matrix generation

---
+++ src/ninemlp/connectivity/__init__.py
@@ -0,0 +1,71 @@
+"""
+
+This package defines methods for producing patterns of connectivity between neuron populations
+
+
+@author: Tom Close
+@date: 23/01/13
+"""
+import numpy as np
+
+def transform_matrix(scale_z=1.0, scale_y=1.0, scale_x=1.0, az=0.0, el=0.0):
+    """
+    Produces a linear transformation matrix for a set of 3D point vectors so that they are scaled 
+    by an ellipsoid from x, y and z axis scalings and azimuth and elevation angles
+    
+    @param scale_x [float): Scale of the x-axis of the transformation matrix
+    @param scale_y [float): Scale of the y-axis of the transformation matrix
+    @param scale_z [float): Scale of the z-axis of the transformation matrix        
+    @param az [float): The azimuth angle of the rotation in the X-Y plane (degrees)
+    @param el [float): The elevation angle of the rotation from the Z plane (degrees)    
+    """
+    # Put the axis scales into a diagonal matrix
+    scale = np.array(((scale_x, 0.0, 0.0), (0.0, scale_y, 0.0), (0.0, 0.0, scale_z)))
+    # Calculate the rotation matrix from the azimuth and elevation angles
+    angles = np.array((az, el)) * (np.pi / 180.0)
+    sin = np.sin(angles)
+    cos = np.cos(angles)
+    rotation = np.array(((cos[0] * cos[1], -sin[0], cos[0] * sin[1]),
+                         (sin[0] * cos [1], cos[0], sin[0] * sin[1]),
+                         (-sin[1], 0, cos[1])))
+    # Combine scale and rotation for final rotation matrix
+    transform = np.dot(rotation, scale)
+#    transform = scale
+    return transform
+
+def prolate_transform(orientation, anistropy=2.0):
+    """
+    Produces a linear transformation matrix for a set of 3D point vectors so that they are scaled 
+    by an ellipsoid from x, y and z axis scalings and azimuth and elevation angles
+    
+    @param scale_x [float): Scale of the x-axis of the transformation matrix
+    @param scale_y [float): Scale of the y-axis of the transformation matrix
+    @param scale_z [float): Scale of the z-axis of the transformation matrix        
+    @param az [float): The azimuth angle of the rotation in the X-Y plane (degrees)
+    @param el [float): The elevation angle of the rotation from the Z plane (degrees)    
+    """
+
+
+if __name__ == "__main__":
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    import numpy.random
+    points = np.mgrid[-1:1:10j, -1:1:10j, -1:1:10j]
+    points = np.transpose(np.reshape(points, (3, -1))) 
+    fig = plt.figure()
+#    ax1 = fig.add_subplot(211, projection='3d')
+#    ax1.set_aspect('equal')
+#    ax1.scatter(points[:,0], points[:, 1], points[:, 2])
+#    ax1.set_xlabel('X Label')
+#    ax1.set_ylabel('Y Label')
+#    ax1.set_zlabel('Z Label')
+    transform_points = np.transpose(np.dot(transform_matrix(2.0, az=45.0, el=90), 
+                                           np.transpose(points)))
+    ax2 = fig.add_subplot(111, projection='3d')
+    ax2.set_aspect('equal')    
+    ax2.scatter(transform_points[:,0], transform_points[:,1], transform_points[:,2])
+    ax2.set_xlabel('X Label')
+    ax2.set_ylabel('Y Label')
+    ax2.set_zlabel('Z Label')
+    plt.show()
+    
