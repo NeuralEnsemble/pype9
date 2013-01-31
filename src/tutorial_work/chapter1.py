30c8dc1d56bf40d0b4a04e3f1803d37f3824cee9
Lisicovas <viktoras.lisicovas@oist.jp>
2013-01-31 17:30:17 +0900

    Created some practice modules to familiarize myself with syntax of python.

---
+++ src/tutorial_work/chapter1.py
@@ -0,0 +1,15 @@
+#Printer function prints out the elements of a 
+def printer (x,y):
+    j=0
+    while j<=y:
+        print x[j]
+        j=j+1
+    return j
+
+
+    
+i=4
+a=["a","s","d","f","g"]
+
+print(printer(a,i))
+
