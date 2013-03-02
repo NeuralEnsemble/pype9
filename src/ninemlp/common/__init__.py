408cc90a692419861902224050b519b925e1d33e
Tom Close <tclose@oist.jp>
2013-03-02 21:52:17 +0900

    Added check for 0Hz poisson spikes setting

--- src/ninemlp/common/__init__.py
+++ src/ninemlp/common/__init__.py
@@ -735,21 +735,23 @@ class Population(object):
         if self.get_cell_type().__name__ != 'SpikeSourceArray':
             raise Exception("'set_poisson_spikes' method can only be used for 'SpikeSourceArray' " \
                             "populations.")
-        mean_interval = 1000 / rate # Convert from Hz to ms
-        stim_range = end_time - start_time
-        if stim_range >= 0.0:
-            estimated_num_spikes = stim_range / mean_interval
-            # Add extra spikes to make sure spike train doesn't stop short
-            estimated_num_spikes = int(estimated_num_spikes + \
-                                       math.exp(-estimated_num_spikes / 10.0) * 10.0)
-            spike_intervals = numpy.random.exponential(mean_interval,
-                                                       size=(self.size, estimated_num_spikes))
-            spike_times = numpy.cumsum(spike_intervals, axis=1) + start_time
-            # FIXME: Should ensure that spike times don't exceed 'end_time' and make it at least until then.
-            self.tset('spike_times', spike_times)
-        else:
-            print "Warning, stimulation start time ({}) is after stimulation end time ({})".\
-                    format(start_time, end_time)
+        # If rate is set to zero do nothing
+        if rate:
+            mean_interval = 1000 / rate # Convert from Hz to ms
+            stim_range = end_time - start_time
+            if stim_range >= 0.0:
+                estimated_num_spikes = stim_range / mean_interval
+                # Add extra spikes to make sure spike train doesn't stop short
+                estimated_num_spikes = int(estimated_num_spikes + \
+                                           math.exp(-estimated_num_spikes / 10.0) * 10.0)
+                spike_intervals = numpy.random.exponential(mean_interval,
+                                                           size=(self.size, estimated_num_spikes))
+                spike_times = numpy.cumsum(spike_intervals, axis=1) + start_time
+                # FIXME: Should ensure that spike times don't exceed 'end_time' and make it at least until then.
+                self.tset('spike_times', spike_times)
+            else:
+                print "Warning, stimulation start time ({}) is after stimulation end time ({})".\
+                        format(start_time, end_time)
 
     def set_spikes(self, spike_times):
         """
