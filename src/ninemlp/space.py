21d11b248a6f6ff18b67a3370b5c8a7f08f4fef8
Tom Close <tclose@oist.jp>
2013-04-04 11:39:36 +0900

    Edited fabios network xml to match original hoc version with the exception of independent assignment of AMPA and NMDA receptors (need to be both included in the same synapse)

--- src/ninemlp/space.py
+++ src/ninemlp/space.py
@@ -17,10 +17,10 @@ from pyNN.random import NumpyRNG
 
 class Perturbed2DGrid(pyNN.space.Grid2D):
 
-    def __init__(self, distr_type, distr_params, self, aspect_ratio=1.0, dx=1.0, dy=1.0, x0=0.0, 
-                                                  y0=0.0, fill_order="sequential", rng=None):
+    def __init__(self, distr_type, distr_params, self, aspect_ratio=1.0, dx=1.0, dy=1.0, x0=0.0,
+                 y0=0.0, z=0.0, fill_order="sequential", rng=None):
         pyNN.space.Grid2D.__init__(self, aspect_ratio=aspect_ratio, dx=dx, dy=dy, x0=x0, y0=y0,
-                                   fill_order=fill_order)
+                                   z=z, fill_order=fill_order)
         self.distr_type = distr_type
         self.distr_params = distr_params
         self.rng = rng or NumpyRNG()
@@ -32,7 +32,7 @@ class Perturbed2DGrid(pyNN.space.Grid2D):
 
 class Perturbed3DGrid(pyNN.space.Grid3D):
 
-    def __init__(self, distr_type, distr_params, aspect_ratioXY=1.0, aspect_ratioXZ=1.0, dx=1.0, 
+    def __init__(self, distr_type, distr_params, aspect_ratioXY=1.0, aspect_ratioXZ=1.0, dx=1.0,
                  dy=1.0, dz=1.0, x0=0.0, y0=0.0, z0=0, fill_order="sequential", rng=None):
         pyNN.space.Grid3D.__init__(self, aspect_ratioXY=aspect_ratioXY,
                                    aspect_ratioXZ=aspect_ratioXZ, dx=dx, dy=dy, dz=dz, x0=x0, y0=y0,
