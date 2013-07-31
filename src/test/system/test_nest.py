f6d7d8d047a30458cade25686b2b1d93c0877cea
Tom Close <tclose@oist.jp>
2013-07-31 13:46:05 +0900

    added basic unit test for loading cells (need to work out how to test that they have loaded properly now)

--- src/test/system/test_nest.py
+++
@@ -1,4 +0,0 @@
-from nine.cells.nest import load_celltype
-CellType = load_celltype('Granule_DeSouza10', '/home/tclose/git/kbrain/xml/cerebellum/ncml/Granule_DeSouza10.xml')
-cell = CellType()
-cell.describe()
