9a59cc4fa83e9dea915d65032f3431e03f47cfd4
Tom Close <tclose@oist.jp>
2013-08-13 18:12:51 +0900

    Added connectors module to handle the selection and instantiation of the projection connectors

---
+++ src/nineline/pyNN/connectors.py
@@ -0,0 +1,89 @@
+from abc import ABCMeta
+import quantities
+import nineml.user_layer
+import pyNN.connectors
+import nineline.pyNN.random
+
+class Connector(object):
+    
+    __metaclass__ = ABCMeta
+    
+    @classmethod
+    def _convert_params(cls, nineml_params, rng):
+        """
+        Converts parameters from lib9ml objects into values with 'quantities' units and or 
+        random distributions
+        """
+        converted_params = {}
+        for name, p in nineml_params.iteritems():
+            if p.unit == 'dimensionless':
+                conv_param = p.value
+            elif p.unit:
+                conv_param = quantities.Quantity(p.value, p.unit)
+            elif isinstance(p.value, str):
+                conv_param = p.value
+            elif isinstance(p.value, nineml.user_layer.RandomDistribution):
+                RandomDistributionClass = getattr(nineline.pyNN.random, 
+                                                  p.value.definition.component.name)
+                conv_param = RandomDistributionClass(p.value.parameters, rng)
+            converted_params[cls.translations[name]] = conv_param 
+        return converted_params
+    
+    def __init__(self, nineml_params, rng=None):
+        # Sorry if this feels a bit hacky (i.e. relying on the pyNN class being the third class in 
+        # the MRO), I thought of a few ways to do this but none were completely satisfactory.
+        PyNNClass = self.__class__.__mro__[2]
+        assert PyNNClass.__module__.startswith('pyNN')
+        PyNNClass.__init__(self, **self._convert_params(nineml_params, rng))
+        
+
+class AllToAllConnector(Connector, pyNN.connectors.AllToAllConnector):
+    
+    translations = {'allowSelfConnections': 'allow_self_connections'}
+
+
+class FixedProbabilityConnector(Connector, pyNN.connectors.FixedProbabilityConnector):
+    
+    translations = {'allowSelfConnections':'allow_self_connections', 'probability':'p_connect'}
+
+
+class DistanceDependentProbabilityConnector(Connector, 
+                                            pyNN.connectors.DistanceDependentProbabilityConnector):
+    
+    translations = {'allowSelfConnections':'allow_self_connections', 
+                    'distanceExpression':'d_expression'}
+
+
+class DisplacementDependentProbabilityConnector(Connector, 
+                                                pyNN.connectors.DisplacementDependentProbabilityConnector):
+    
+    translations = {'allowSelfConnections':'allow_self_connections', 
+                    'displacementExpression':'disp_function'}
+
+
+class FixedNumberPostConnector(Connector, pyNN.connectors.FixedNumberPostConnector):
+    
+    translations = {'allowSelfConnections':'allow_self_connections', 'number':'n'}
+
+
+class FixedNumberPreConnector(Connector, pyNN.connectors.FixedNumberPreConnector):
+    
+    translations = {'allowSelfConnections':'allow_self_connections', 'number':'n'}
+
+
+class OneToOneConnector(Connector, pyNN.connectors.OneToOneConnector):
+    
+    translations = {}
+
+
+class SmallWorldConnector(Connector, pyNN.connectors.SmallWorldConnector):
+    
+    translations = {'allowSelfConnections':'allow_self_connections', 'degree': 'degree', 
+                    'rewiring':'rewiring', 'numberOfConnections':'n_connections'}
+
+
+class CloneConnector(Connector, pyNN.connectors.CloneConnector):
+    
+    translations = {'projection':'reference_projection'}            
+
+        
