19f2569765b4f01f27f9e5b5acdca5cd7b76f596
Tom Close <tclose@oist.jp>
2013-08-19 11:52:19 +0900

    fixed up typo with nest.StaticSynapse

--- src/nineline/pyNN/nest/synapses.py
+++ src/nineline/pyNN/nest/synapses.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 import pyNN.nest.standardmodels.synapses
-from nineline.pyNN.common.synapses import Synapse
+from nineline.pyNN.common.synapses import StaticSynapse
 
-class StaticSynapse(Synapse, pyNN.nest.standardmodels.synapses.StaticSynapse): pass
+class StaticSynapse(StaticSynapse, pyNN.nest.standardmodels.synapses.StaticSynapse): pass
     
 # class ElectricalSynapse(Synapse, pyNN.nest.standardmodels.synapses.ElectricalSynapse): pass
