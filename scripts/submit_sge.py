6669ad08311c70408072a4c89b21ca8bfc8dea95
Tom Close <tclose@oist.jp>
2014-06-02 09:31:55 +0900

    refactored folder structure to make it more standard

--- scripts/submit_sge.py
+++ scripts/submit_sge.py
@@ -2,8 +2,8 @@ import sys
 from nineline.hpc.sge import SGESubmitter
 
 if len(sys.argv) < 2:
-    raise Exception("At least one argument (the script name to submit to the que) should be "
-                    "passed to que_sge.py")
+    raise Exception("At least one argument (the script name to submit to the "
+                    "que) should be passed to que_sge.py")
 submitter = SGESubmitter(sys.argv[1])
 args = submitter.parse_arguments(sys.argv[2:])
 submitter.submit(args)
