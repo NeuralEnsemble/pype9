57607578c10f316da1acc2746a7f202d9ad3e3e1
Tom Close <tclose@oist.jp>
2014-07-12 08:33:29 +0900

    touched up test_case

--- test/unittests/test_9ml_conversion.py
+++ test/unittests/test_9ml_conversion.py
@@ -7,20 +7,14 @@ from lxml import etree
 import nineml.extensions.biophysical_cells
 from nineline.cells import Tree
 
-nineml_in = os.path.join(os.path.dirname(__file__), '..', 'data', '9ml',
+nineml = os.path.join(os.path.dirname(__file__), '..', 'data', '9ml',
                          'Golgi_Solinas08.9ml')
-nineml_out = os.path.join(os.path.dirname(__file__), '..', 'data', '9ml',
-                         'Golgi_Solinas08-out.9ml')
-
 
 class Test9mlConversion(unittest.TestCase):
 
     def test_9ml_convertion(self):
-        models = nineml.extensions.biophysical_cells.parse(nineml_in)
-        model = next(models.itervalues())
-        tree = Tree.from_9ml(model)
-        etree.ElementTree(tree.to_9ml().to_xml()).write(
-                 nineml_out,
-                 encoding="UTF-8",
-                 pretty_print=True,
-                 xml_declaration=True)
+        models = nineml.extensions.biophysical_cells.parse(nineml)
+        model9ml = next(models.itervalues())
+        tree = Tree.from_9ml(model9ml)
+        new_model9ml = tree.to_9ml()
+        self.assertEqual(model9ml, new_model9ml)
