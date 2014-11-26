38b505e248d2d61b359182daa609477b8049541a
Tom Close <tclose@oist.jp>
2014-11-26 11:55:45 +0900

    added units and dimensions to test xml files

--- test/unittests/test_nest_build.py
+++ test/unittests/test_nest_build.py
@@ -161,12 +161,12 @@ from nineline.cells.code_gen.nest import CodeGenerator
 import nineml
 from os.path import dirname, join, abspath, realpath
 build_dir = join(dirname(__file__), 'build')
-test_dir = abspath(join(dirname(realpath(nineml.__file__)), '..', 'test',
+test_dir = abspath(join(dirname(__file__), '..', 'data',
                         'xml'))
-component_file = join(test_dir, 'neurons', 'HodgkinHuxleyModified.xml')
+component_file = join(test_dir, 'HodgkinHuxleyModified.xml')
 comp = nineml.read(component_file)['HodgkinHuxleyModified']
-initial_state_file = join(dirname(__file__), '..', '..',
-                                  'examples', 'HodgkinHuxleyInitialState.xml')
+# initial_state_file = join(dirname(__file__), '..', '..',
+#                                   'examples', 'HodgkinHuxleyInitialState.xml')
 code_generator = CodeGenerator()
 code_generator.generate(component_file, 0.0,  # initial_state_file,
                         build_mode='generate_only')
