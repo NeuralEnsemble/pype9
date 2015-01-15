9a6e7c910f51c496504561f4b57cdaa71349f226
Russell <russelljarvis42@gmail.com>
2015-01-15 17:00:16 +0900

    fixed missing import os statement.

--- cerebc_nmodl_to_xml.py
+++ cerebc_nmodl_to_xml.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env
 import btmorph
 import nineml
 import nineline
@@ -9,9 +9,10 @@ import nineline.arguments
 import nineline.cells.build
 import nineline.cells.code_gen
 #import nineline.test
-#import os
-#os.path.join(in_path, '/test/unittests')
-os.chdir(os.getcwd()+'/test/unittests')
-print os.getcwd()
-execfile('test_neuron_build.py')
+import os
+os.chdir(os.getcwd()+'/test/unittests/')
+
+
+#execfile('test_neuron_build.py')
+#test neuron build means building neuron code from XML.
 execfile('test_neuron_import.py')
