73d3843f73acc7b3a52a1c2ebe9f0e4907991b2b
Tom Close <tclose@oist.jp>
2015-02-05 15:15:49 +0900

    added simulator-specific import files that import all the relevant objects into a single module

---
+++ pype9/neuron.py
@@ -0,0 +1,5 @@
+from .cells.neuron import Pype9Cell, Pype9CellMetaClass  # @UnusedImport
+from .pynn_interface.network.neuron import Network  # @UnusedImport
+from .pynn_interface.population.neuron import Population  # @UnusedImport
+from .pynn_interface.projection.neuron import Projection  # @UnusedImport
+from .pynn_interface.synapses.neuron import StaticSynapse  # @UnusedImport
