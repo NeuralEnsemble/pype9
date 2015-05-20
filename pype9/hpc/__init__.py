47aea5958f76a8c2bf85f9a55dbfaefcbe9a3462
Tom Close <tclose@oist.jp>
2015-05-20 17:48:05 +0900

    removed hpc code as it has been moved to its own repository

--- pype9/hpc/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-  Author: Thomas G. Close (tclose@oist.jp)
-  Copyright: 2012-2014 Thomas G. Close.
-  License: This file is part of the "NineLine" package, which is released under
-           the MIT Licence, see LICENSE for details.
-"""
