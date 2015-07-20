cd9f9287e9c3857d6a9dc39901eda2734c5058a8
Tom Close <tclose@oist.jp>
2015-07-20 13:19:05 +1000

    added code to convert units

---
+++ pype9/nest/__init__.py
@@ -0,0 +1,4 @@
+from nineml import units as un
+
+
+units = [un.ms, un.mV, un.pA, un.mM, un.uF, un.um, un.uS, un.degC, un.cd]
---
+++ pype9/neuron/__init__.py
@@ -0,0 +1,6 @@
+from nineml import units as un
+
+
+units = [un.ms, un.mV, un.mA / un.cm ** 2, un.nA, un.mM, un.uF / un.cm ** 2,
+         un.um, un.S / un.cm ** 2, un.uS, un.ohm * un.cm, un.ohm, un.degC,
+         un.cd]
