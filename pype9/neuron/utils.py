31e24d327828c7596dac3daa031c6ab332d9a9e2
Tom Close <tclose@oist.jp>
2015-07-26 12:29:38 +1000

    added unit converters for the neuron and nest

--- pype9/neuron/utils.py
+++ pype9/neuron/utils.py
@@ -4,6 +4,5 @@ import pype9.common.utils
 
 class UnitConverter(pype9.common.utils.UnitConverter):
 
-    basis = [un.ms, un.mV, un.mA / un.cm ** 2, un.nA, un.mM,
-             un.uF / un.cm ** 2, un.um, un.S / un.cm ** 2, un.uS,
-             un.ohm * un.cm, un.ohm, un.degC, un.cd]
+    basis = [un.ms, un.mV, un.nA, un.mM, un.uF, un.um, un.uS, un.ohm,
+             un.K, un.cd]
