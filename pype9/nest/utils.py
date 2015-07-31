219af168313fd0d72d150ec10c8b17b26b0e9440
Tom Close <tclose@oist.jp>
2015-08-01 01:01:04 +1000

    fixed up name of UnitAssigner in nest package

--- pype9/nest/utils.py
+++ pype9/nest/utils.py
@@ -2,7 +2,7 @@ from nineml import units as un
 from pype9.base.utils import BaseUnitAssigner
 
 
-class _DimensionToUnitMapper(BaseUnitAssigner):
+class UnitAssigner(BaseUnitAssigner):
 
     basis = [un.ms, un.mV, un.pA, un.mM, un.uF, un.um, un.uS, un.K, un.cd]
     compounds = []
