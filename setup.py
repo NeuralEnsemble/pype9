b9f89061a2fb1211f85502535f42b56270b3dd0e
Tom Close <tclose@oist.jp>
2015-11-26 10:12:39 +1100

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages  # @UnresolvedImport
 
 setup(
-    name="9Line",
+    name="PyPe9",
     version="0.1dev",
     package_data={'pype9': ['pype9', 'pype9/*.py', 'pype9/cells',
-                               'pype9/cells/code_gen','pype9/cells/build',
-                               'pype9/importer','pype9/pyNN','pype9/hpc',
-                               'pype9/test','pype9/test/unittests',
-                               'pype9/morphology']},
-    packages = find_packages(),
-    author="Thomas G. Close (tclose@oist.jp), Ivan Raikov (raikov@oist.jp)",
+                            'pype9/cells/code_gen', 'pype9/cells/build',
+                            'pype9/importer', 'pype9/pyNN', 'pype9/hpc',
+                            'pype9/test', 'pype9/test/unittests',
+                            'pype9/morphology']},
+    packages=find_packages(),
+    author="Thomas G. Close",
     # add your name here if you contribute to the code
-    author_email="tclose@oist.jp, raikov@oist.jp",
-    description="A NINEml software pipeLINE that reads and simulates networks of detailed neuronal models described in 9ml.",
+    author_email="tom.g.close@gmail.com",
+    description=("A NINEml software pipeLINE that reads and simulates networks"
+                 " of detailed neuronal models described in 9ml."),
     long_description=open("README.md").read(),
     license="The MIT License (MIT)",
-    keywords="PipeLINE NINEml computational neuroscience modeling interoperability XML",
-    url="",
+    keywords=("Python pipeline NineML computational neuroscience modeling "
+              "interoperability XML"),
+    url="http://github.com/CNS-OIST/PyPe9",
     classifiers=['Development Status :: initial stages',
                  'Environment :: Console',
                  'Intended Audience :: Science/Research',
@@ -27,9 +29,6 @@ setup(
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 2',
                  'Topic :: Scientific/Engineering'],
-    install_requires=['lxml', 'ply', 'numpy', 'quantities'],
-    #cannot add the the requirements btmorph, and nineml above, because if those install requirements are listed 
-    #Then this install script searches pip for those packages. btmorph is not a pip package, and it is not desirable to overwrite specific development versions 
-    #of nineml with the pip stable version.
+    install_requires=['nineml', 'pyNN'],
     tests_require=['nose']
 )
