d9182e041948e996fce55afa120f90d6262b1561
Tom Close <tclose@oist.jp>
2016-07-26 07:52:45 +1000

    simplified test directory structure

--- test/compare/nest/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-aclocal \
-&& automake --add-missing \
-&& autoconf
-rm -r build/*
-mkdir build
-cd build
-CPPFLAGS=-DDEBUG CFLAGS='-g3 -O0' CXXFLAGS='-g3 -O0' ../configure --prefix=`pwd`/..
-make install
