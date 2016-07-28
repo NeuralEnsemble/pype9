6f9fc7f371f92e70baebd0d860ec6ceb6b7df3e9
Tom Close <tclose@oist.jp>
2016-07-29 08:41:15 +1000

    copied install scripts from pyNN and fixed up travis.yml

---
+++ ci/install_nest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Adapted from install scritp in pyNN
+
+set -e  # stop execution in case of errors
+
+export NEST_VERSION="2.10.0"
+export NEST="nest-$NEST_VERSION"
+pip install cython
+if [ ! -f "$HOME/$NEST_VERSION/configure" ]; then
+    wget https://github.com/nest/nest-simulator/releases/download/v$NEST_VERSION/$NEST.tar.gz -O $HOME/$NEST.tar.gz;
+    pushd $HOME;
+    tar xzf $NEST.tar.gz;
+    popd;
+else
+    echo 'Using cached version of NEST sources.';
+fi
+mkdir -p $HOME/build/$NEST
+pushd $HOME/build/$NEST
+if [ ! -f "$HOME/build/$NEST/config.log" ]; then
+    export VENV=`python -c "import sys; print sys.prefix"`;
+    $HOME/$NEST/configure --with-mpi --prefix=$VENV;
+    make;
+else
+    echo 'Using cached NEST build directory.';
+    echo "$HOME/$NEST";
+    ls $HOME/$NEST;
+    echo "$HOME/build/$NEST";
+    ls $HOME/build/$NEST;
+fi
+make install
+popd
