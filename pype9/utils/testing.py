9f38b3c95936eb5e9a6b281ddea084b4768881ce
Tom Close <tclose@oist.jp>
2016-08-08 22:18:20 +1000

    attempting to fix up reference Brunel network

--- pype9/utils/testing.py
+++ pype9/utils/testing.py
@@ -652,7 +652,7 @@ class ReferenceBrunel2000(object):
 
         # normalize synaptic current so that amplitude of a PSP is J
         J_unit = cls.compute_normalised_psr(cls.tauMem,
-                                            cls.R, cls.tauSyn) / 1000.0
+                                            cls.R, cls.tauSyn)
         J_ex = cls.J / J_unit
         J_in = -g * J_ex
 
