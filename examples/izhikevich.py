fixed up typos and removed explicit scaling of J_unit (now handled by units)

examples/izhikevich.py
@@ -73,8 +73,7 @@ if __name__ == "__main__":
             "--reference and --fast_spiking options cannot be used together as"
             " there is no reference implementation for the fast-spiking model")
 
-
-   # Set the random seed
+    # Set the random seed
     np.random.seed(args.seed)
     seeds = np.asarray(
         np.floor(np.random.random(len(args.simulators)) * 1e5), dtype=int)
@@ -174,7 +173,7 @@ if __name__ == "__main__":
     # Needs to be imported after the args.save_fig argument is parsed to
     # allow the backend to be set
     from matplotlib import pyplot as plt  # @IgnorePep8
- 
+
     print "Plotting the results"
     plt.figure(figsize=args.figsize)
     if args.fast_spiking: