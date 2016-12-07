beb12177a8e9356da4032db4100d4a29c3cdc322
Tom Close <tclose@oist.jp>
2016-12-07 19:41:15 +1100

    reinstated cache

--- prereq/install_nest.sh
+++ prereq/install_nest.sh
@@ -6,27 +6,27 @@ set -e  # stop execution in case of errors
 export NEST_VERSION="2.10.0"
 export NEST="nest-$NEST_VERSION"
 pip install cython
-#if [ ! -f "$HOME/$NEST_VERSION/configure" ]; then
+if [ ! -f "$HOME/$NEST_VERSION/configure" ]; then
     wget https://github.com/nest/nest-simulator/releases/download/v$NEST_VERSION/$NEST.tar.gz -O $HOME/$NEST.tar.gz;
     pushd $HOME;
     tar xzf $NEST.tar.gz;
     popd;
-#else
-#    echo 'Using cached version of NEST sources.';
-#fi
+else
+    echo 'Using cached version of NEST sources.';
+fi
 mkdir -p $HOME/build/$NEST
 pushd $HOME/build/$NEST
-#if [ ! -f "$HOME/build/$NEST/config.log" ]; then
+if [ ! -f "$HOME/build/$NEST/config.log" ]; then
     export VENV=`python -c "import sys; print sys.prefix"`;
     echo "VENV: $VENV"
     $HOME/$NEST/configure --with-mpi --prefix=$VENV;
     make;
-#else
-#    echo 'Using cached NEST build directory.';
-#    echo "$HOME/$NEST";
-#    ls $HOME/$NEST;
-#    echo "$HOME/build/$NEST";
-#    ls $HOME/build/$NEST;
-#fi
+else
+    echo 'Using cached NEST build directory.';
+    echo "$HOME/$NEST";
+    ls $HOME/$NEST;
+    echo "$HOME/build/$NEST";
+    ls $HOME/build/$NEST;
+fi
 make install
 popd
--- prereq/install_neuron.sh
+++ prereq/install_neuron.sh
@@ -4,23 +4,23 @@
 set -e  # stop execution in case of errors
 
 export NRN_VERSION="nrn-7.3"
-#if [ ! -f "$HOME/$NRN_VERSION/configure" ]; then
+if [ ! -f "$HOME/$NRN_VERSION/configure" ]; then
     wget http://www.neuron.yale.edu/ftp/neuron/versions/v7.3/$NRN_VERSION.tar.gz -O $HOME/$NRN_VERSION.tar.gz;
     pushd $HOME;
     tar xzf $NRN_VERSION.tar.gz;
     popd;
-#else
-    #echo 'Using cached version of NEURON sources.';
-#fi
+else
+    echo 'Using cached version of NEURON sources.';
+fi
 mkdir -p $HOME/build/$NRN_VERSION
 pushd $HOME/build/$NRN_VERSION
 export VENV=`python -c "import sys; print sys.prefix"`;
-#if [ ! -f "$HOME/build/$NRN_VERSION/config.log" ]; then
+if [ ! -f "$HOME/build/$NRN_VERSION/config.log" ]; then
     $HOME/$NRN_VERSION/configure --with-paranrn --with-nrnpython --prefix=$VENV --without-iv;
     make;
-#else
-#    echo 'Using cached NEURON build directory.';
-#fi
+else
+    echo 'Using cached NEURON build directory.';
+fi
 make install
 cd src/nrnpython
 python setup.py install
