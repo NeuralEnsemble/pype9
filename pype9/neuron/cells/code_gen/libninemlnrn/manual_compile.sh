662a0cc390558ef72b1ca0c8ee3f9d69497b746b
Tom Close <tclose@oist.jp>
2016-12-11 15:51:57 +1100

    added more informative error message to manual_compile

--- pype9/neuron/cells/code_gen/libninemlnrn/manual_compile.sh
+++ pype9/neuron/cells/code_gen/libninemlnrn/manual_compile.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# For manual compilation of libninemlnrn if the setup.py fails or neuron is updated
+# For manual compilation of libninemlnrn if the setup.py fails or neuron is reinstalled
 
 if [ "$CC" == "" ]; then
-	echo "Please set the environment variable CC to the C-compiler used by nrnivmodl"
+	echo "Please set the environment variable CC to the C-compiler used by nrnivmodl."
+	echo "This can be found by locating nrnmech_makefile and finding the value it sets for CC"
+	echo "If you have installed GNU Scientific Library with a non-standard prefix then "
+	echo "please also set the WITH_GSL environment variable to the GSL prefix"
+	exit
 fi
 
 if [ "$WITH_GSL" != "" ]; then
