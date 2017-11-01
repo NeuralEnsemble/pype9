7b1b69a96667b896104c5da91abe86b0e6d1debf
Tom Close <tom.g.close@gmail.com>
2017-11-01 15:24:32 +1100

    added Python 3 to travis testing

--- ci/install_nest.sh
+++ ci/install_nest.sh
@@ -1,51 +1,62 @@
 #!/bin/bash
 # Adapted from similar install script in pyNN (https://github.com/NeuralEnsemble/PyNN)
 
-	
-export NEST_VERSION="2.12.0"
-export NEST="nest-$NEST_VERSION"
+if [ -z "$1" ]; then
+	echo "Please provide NEST version to install script"
+	return
+fi
 
-# Remove cache because it is causing errors until the previous build runs successfully
-#rm -rf $HOME/$NEST
-#rm -rf $HOME/build/$NEST
+NEST_VERSION=$1
+NEST="nest-$NEST_VERSION"
+export NEST_SRC_DIR=$HOME/$NEST
+export NEST_BUILD_DIR=$HOME/build/$NEST
+
+if [ "$2" == 'purge' ]; then
+	# Remove cache because it is causing errors until the previous build runs successfully
+	rm -rf $NEST_SRC_DIR
+	rm -rf $NEST_BUILD_DIR
+fi
 
 set -e  # stop execution in case of errors
 
-pip install cython
-if [ ! -f "$HOME/$NEST/CMakeLists.txt" ]; then
-    wget https://github.com/nest/nest-simulator/releases/download/v$NEST_VERSION/$NEST.tar.gz -O $HOME/$NEST.tar.gz;
+if [ ! -f "$NEST_SRC_DIR/CMakeLists.txt" ]; then
+    wget https://github.com/nest/nest-simulator/releases/download/v$NEST_VERSION/$NEST.tar.gz \
+     -O $HOME/$NEST.tar.gz;
     pushd $HOME;
     tar xzf $NEST.tar.gz;
     popd;
 else
     echo 'Using cached version of NEST sources.';
 fi
-mkdir -p $HOME/build/$NEST
-pushd $HOME/build/$NEST
+
+mkdir -p $NEST_BUILD_DIR
+pushd $NEST_BUILD_DIR
 
 # Get Python installation information
-export VENV=$(python -c "import sys; print(sys.prefix)");
-export PYLIB_DIR=$(python -c 'from distutils import sysconfig; print(sysconfig.get_config_var("LIBDIR"))');
-export PYINC_DIR=$(python -c 'from distutils import sysconfig; print(sysconfig.get_config_var("INCLUDEDIR"))');
-export PYLIB_NAME=$(python -c 'from distutils import sysconfig; print(".".join(sysconfig.get_config_var("LIBRARY").split(".")[:2]))').so;
-export PYVER=$(python -c 'import sys; print("{}.{}".format(*sys.version_info[:2]))');
-export PYLIBRARY=$PYLIB_DIR/$PYLIB_NAME
+VENV=$(python -c "import sys; print(sys.prefix)");
+PYLIB_DIR=$(python -c 'from distutils import sysconfig; print(sysconfig.get_config_var("LIBDIR"))');
+PYINC_DIR=$(python -c 'from distutils import sysconfig; print(sysconfig.get_config_var("INCLUDEDIR"))');
+PYLIB_NAME=$(python -c 'from distutils import sysconfig; print(".".join(sysconfig.get_config_var("LIBRARY").split(".")[:2]))').so;
+PYVER=$(python -c 'import sys; print("{}.{}".format(*sys.version_info[:2]))');
+PYLIBRARY=$PYLIB_DIR/$PYLIB_NAME
 
-# To reset cache after updates
-# rm $HOME/build/$NEST/config.log
+# Install cython
+pip install cython
 
-if [ ! -d "$HOME/build/$NEST/CMakeFiles" ]; then
+if [ ! -d "$NEST_BUILD_DIR/CMakeFiles" ]; then
     echo "VENV: $VENV"
     echo "Python Library: $PYLIBRARY"
     echo "Python include dir: $PYINC_DIR"
-    cmake -Dwith-mpi=ON -DPYTHON_LIBRARY=$PYLIBRARY -DPYTHON_INCLUDE_DIR=$PYINC_DIR/python$PYVER -DCMAKE_INSTALL_PREFIX=$VENV $HOME/$NEST;
+    cmake -Dwith-mpi=ON -DPYTHON_LIBRARY=$PYLIBRARY \
+     -DPYTHON_INCLUDE_DIR=$PYINC_DIR/python$PYVER \
+     -DCMAKE_INSTALL_PREFIX=$VENV $NEST_SRC_DIR;
     make;
 else
-    echo 'Using cached NEST build directory.';
-    echo "$HOME/$NEST";
-    ls $HOME/$NEST;
-    echo "$HOME/build/$NEST";
-    ls $HOME/build/$NEST;
+    echo 'Using cached NEST build and install directories.';
+    echo "$NEST_SRC_DIR";
+    ls $NEST_SRC_DIR;
+    echo "$NEST_BUILD_DIR";
+    ls $NEST_BUILD_DIR;
 fi
 make install
 popd
--- ci/install_neuron.sh
+++ ci/install_neuron.sh
@@ -3,11 +3,23 @@
 
 set -e  # stop execution in case of errors
 
-export NRN_VERSION="7.5"
-export NRN=nrn-$NRN_VERSION
+if [ -z "$1" ]; then
+	echo "Please provide Neuron version to install script"
+	return
+fi
 
+NRN_VERSION=$1
+NRN=nrn-$NRN_VERSION
+export NEURON_SRC_DIR=$HOME/$NRN
+export NEURON_BUILD_DIR=$HOME/build/$NRN
 
-if [ ! -f "$HOME/$NRN/configure" ]; then
+if [ "$2" == 'purge' ]; then
+	# Remove cache because it is causing errors until the previous build runs successfully
+	rm -rf $NEURON_SRC_DIR
+	rm -rf $NEURON_BUILD_DIR
+fi
+
+if [ ! -f "$NEURON_SRC_DIR/configure" ]; then
     wget http://www.neuron.yale.edu/ftp/neuron/versions/v$NRN_VERSION/$NRN.tar.gz -O $HOME/$NRN.tar.gz;
     pushd $HOME;
     tar xzf nrn-$NRN_VERSION.tar.gz;
@@ -16,15 +28,13 @@ else
     echo 'Using cached version of NEURON sources.';
 fi
 
-mkdir -p $HOME/build/$NRN
-pushd $HOME/build/$NRN
+mkdir -p $NEURON_BUILD_DIR
+pushd $NEURON_BUILD_DIR
 export VENV=`python -c "import sys; print(sys.prefix)"`;
 
-# To reset cache after updates
-# rm $HOME/build/$NRN/config.log
-
-if [ ! -f "$HOME/build/$NRN/config.log" ]; then
-    $HOME/$NRN/configure --with-paranrn --with-nrnpython=$VENV/bin/python --prefix=$VENV --disable-rx3d --without-iv;
+if [ ! -f "$NEURON_BUILD_DIR/config.log" ]; then
+    $NEURON_SRC_DIR/configure --with-paranrn --with-nrnpython=$VENV/bin/python \
+     --prefix=$VENV --disable-rx3d --without-iv;
     make;
 else
     echo 'Using cached NEURON build directory.';
