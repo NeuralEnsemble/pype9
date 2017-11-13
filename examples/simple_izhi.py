b1e7857d36100f20f1f66a9809942b3cdfd80e27
Tom Close <tom.g.close@gmail.com>
2017-11-13 16:38:00 +1100

    added initial regime to simple izhi

--- examples/simple_izhi.py
+++ examples/simple_izhi.py
@@ -1,21 +1,18 @@
 import ninemlcatalog
 from nineml import units as un
-from pype9.simulate.nest import CellMetaClass, Simulation
+from pype9.simulate.nest import CellMetaClass, Simulation  # Alternatively load the neuron class from pype9.simulator.neuron import CellMetaClass, simulate @IgnorePep8
 from pype9.plot import plot
 import pype9.utils.print_logger  # @UnusedImport
-# Alternatively load the neuron class
-# from pype9.simulator.neuron import CellMetaClass, simulate
-
 
 # Compile and load cell class
 Izhikevich = CellMetaClass(
-    ninemlcatalog.load('//neuron/Izhikevich#Izhikevich'))
+    ninemlcatalog.load('//neuron/Izhikevich#IzhikevichFastSpiking'))
 
 # Create and run the simulation
 with Simulation(dt=0.01 * un.ms) as sim:
     izhi = Izhikevich(
-        ninemlcatalog.load('//neuron/Izhikevich#SampleIzhikevich'),
-        U=-14.0 * un.mV / un.ms, V=-65 * un.mV)
+        ninemlcatalog.load('//neuron/Izhikevich#SampleIzhikevichFastSpiking'),
+        regime_='subthreshold')
     izhi.record('U')
     izhi.record('V')
     sim.run(100 * un.ms)
